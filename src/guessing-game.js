f1b37d2a142f20a1b02fde2d5ad0b07bad520f73
Shellby <shellby666@gmail.com>
2017-02-04 21:51:56 +0300

    guessingGame/sources

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
+
 }
 
 module.exports = GuessingGame;
